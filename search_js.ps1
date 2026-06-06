5c1df1a48dc8441af6fb7e33850d04c2c4b376f7
Juan <lenguyenminh505@gmail.com>
2026-06-06 23:37:38 +0700

    update

--- search_js.ps1
+++
@@ -1,8 +0,0 @@
-$content = Get-Content 'd:\Webbook\bookshop\public\build\assets\app-ChAdZdjz.js' -Raw
-$idx = $content.IndexOf('data-cart-dec')
-Write-Host "data-cart-dec at: $idx"
-if ($idx -ge 0) {
-    $start = [Math]::Max(0, $idx - 30)
-    $len = [Math]::Min(300, $content.Length - $start)
-    Write-Host $content.Substring($start, $len)
-}
